921ed65d50e717b75683c8540d6563c2f18f90f9
Akai66 <869896679@qq.com>
2023-10-27 17:23:39 +0800

    fix: add waitgroup

--- cmd/design-pattern/singleton/singleton.go
+++ cmd/design-pattern/singleton/singleton.go
@@ -3,12 +3,16 @@ package main
 import (
 	"fmt"
 	"github.com/Akai66/pandora/internal/design-pattern/singleton"
+	"sync"
 )
 
 const pandoraMysqlDns = "root:iam59!z$@tcp(127.0.0.1:3306)/pandora?charset=utf8"
 
+var wg sync.WaitGroup
+
 func main() {
 	for i := 0; i < 10; i++ {
+		wg.Add(1)
 		go getIns(i, singleton.GetMysqlFunc{
 			Name: "Lock",
 			Func: singleton.GetMysqlInsOrByLock,
@@ -18,10 +22,11 @@ func main() {
 		//	Func: singleton.GetMysqlInsOrByOnce,
 		//})
 	}
-
+	wg.Wait()
 }
 
 func getIns(id int, f singleton.GetMysqlFunc) {
+	defer wg.Done()
 	ins, err := f.Func(pandoraMysqlDns)
 	if err != nil {
 		fmt.Println(err)
